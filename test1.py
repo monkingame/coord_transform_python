2c8fe0dbebe5b52fbd81084cbb58322555f98271
sunshouhua <65739056@qq.com>
2023-04-23 08:53:40 +0800

    t

--- test1.py
+++ test1.py
@@ -1,5 +1,11 @@
-from coordTransform_utils import Geocoding
+# from coordTransform_utils import Geocoding
+import coordTransform_utils as util
 
 lng = 118.07400059168339
 lat = 36.845435015941064
 
+result2 = util.bd09_to_gcj02(lng, lat)
+result5 = util.bd09_to_wgs84(lng, lat)
+
+print('bd09_to_gcj02 bd09_to_wgs84')
+print(result2, result5)
