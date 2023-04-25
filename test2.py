32fb8b215bac614607ea4674fc089924e9864aa0
sunshouhua <65739056@qq.com>
2023-04-25 17:04:45 +0800

    a

--- test2.py
+++ test2.py
@@ -9,7 +9,7 @@ import coordTransform_utils as util
 excel_path=r'C:\Users\sun\Downloads\坐标-测试.xlsx'
 
 df = pd.read_excel(excel_path,
-                   usecols=['经营店铺名称','唯一编码', '地理经度', '地理纬度'],
+                   usecols=['经营店铺名称','唯一编码', '地理经度', '地理纬度','lng','lat'],
                    converters={'唯一编码':str},)
 
 line_count=df.shape[0]
@@ -20,6 +20,8 @@ for index,row in df.iterrows():
     id=row['唯一编码']
     lng=row['地理经度']
     lat=row['地理纬度']
+    lng_save=row['lng']
+    lat_save=row['lat']
 
     # if pd.isna(id) or (len(id)==0): 
     #     pass
@@ -28,7 +30,12 @@ for index,row in df.iterrows():
     #     gaode=util.bd09_to_gcj02(lng,lat)
     #     print(index, id,name,gaode[0],gaode[1])
     if not pd.isna(id) and not pd.isna(lng) and not pd.isna(lat):
-        print(index, id,name,lng,lat)
+        # print(index, id,name,lng,lat)
+        baidu=util.gcj02_to_bd09(lng,lat)
+        # print(index, id,name,baidu[0],baidu[1])
+        df.at[index, 'lng'] = baidu[0]
+        df.at[index, 'lat'] = baidu[1]
 
+df.to_excel(excel_path, index=False)
 
 
