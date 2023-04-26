1f8cba7a758491c945f1fb5eb002193705e52c3e
sunshouhua <65739056@qq.com>
2023-04-26 09:28:09 +0800

    a

--- read_gaode.py
+++ read_gaode.py
@@ -9,7 +9,7 @@ import coordTransform_utils as util
 import csv
 
 # excel_path=r'C:\Users\sun\OneDrive\数字淄博\开发-淄博烧烤\20230425-数据\20230425-坐标分别整理\2-整理\1-599-百度坐标系.xlsx'
-excel_path=r'C:\Users\sun\OneDrive\数字淄博\开发-淄博烧烤\20230425-数据\20230425-坐标分别整理\2-整理\1200-1799-百度坐标系-2.xlsx'
+excel_path=r'C:\Users\sun\OneDrive\数字淄博\开发-淄博烧烤\20230425-数据\20230425-坐标分别整理\2-整理\3600-4229.xlsx'
 
 # df = pd.read_excel(excel_path,
 #                    usecols=['经营店铺名称','唯一编码', '地理经度', '地理纬度','lng','lat'],
@@ -34,20 +34,16 @@ for index,row in df.iterrows():
     lat_save=row['lat']
 
     if not pd.isna(id) and not pd.isna(lng) and not pd.isna(lat):
-        # print(index, id,name,lng,lat)
-        gcj=util.bd09_to_gcj02(lng,lat)
-        # print(index, id,name,baidu[0],baidu[1])
-        df.at[index, 'lng'] = gcj[0]
-        df.at[index, 'lat'] = gcj[1]
-        # if index<100:
-        #     print(id,name,gcj[0],gcj[1],lng,lat)
-        data.append([f'{id},{gcj[0]},{gcj[1]}'])
+        # gcj=util.bd09_to_gcj02(lng,lat)
+        # df.at[index, 'lng'] = gcj[0]
+        # df.at[index, 'lat'] = gcj[1]
+        data.append([f'{id},{lng},{lat}'])
 
 # df.to_excel(excel_path, index=False)
 
 print(len(data))
 
-with open(r'C:\Users\sun\Downloads\1200-1799.csv', 'w', newline='') as file:
+with open(r'C:\Users\sun\Downloads\3600-4229.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(data)
     # pass
