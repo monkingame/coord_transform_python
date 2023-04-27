288d7dd883ff59ba00f5ab9cc6755312576e4707
sunshouhua <65739056@qq.com>
2023-04-27 21:46:35 +0800

    a

--- excel_strip.py
+++ excel_strip.py
@@ -3,10 +3,24 @@
 # 将excel内单元格内所有的空格去掉，包括中间的
 
 import pandas as pd
-import coordTransform_utils as util
-import csv
+import re
 
 excel_path=r'C:\Users\sun\OneDrive\数字淄博\开发-淄博烧烤\20230427-数据\市直及区县停车场和厕所\整理-4-区县.xlsx'
 
 df = pd.read_excel(excel_path,)
-df = df.fillna('')
+# df = df.fillna('')
+
+for index,row in df.iterrows():
+    index=row['序号']
+    depart=re.sub(r"\s+", "",row['单位'])
+    addr=re.sub(r"\s+", "",row['地址'])
+    park=re.sub(r"\s+", "",row['停车场'])
+    toilet=re.sub(r"\s+", "",row['厕所'])
+    # print(index,depart,addr,park,toilet)
+    df.at[index, '单位'] = depart
+    df.at[index, '地址'] = addr
+    df.at[index, '停车场'] = park
+    df.at[index, '厕所'] = toilet
+
+df.to_excel(excel_path, index=False)
+
