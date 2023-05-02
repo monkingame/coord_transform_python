55e5d4578feb7783ecf07bd4bd17847a128e99b8
sunshouhua <65739056@qq.com>
2023-05-02 16:30:35 +0800

    a

--- read_0501_wenjuanxing_01.py
+++ read_0501_wenjuanxing_01.py
@@ -95,19 +95,81 @@ import coordTransform_utils as util
 
 ###############################################################################
 
-all_wenjuanxing=r'C:\Users\sun\OneDrive\数字淄博\开发-淄博烧烤\20230502-数据\20230502-1430-问卷星全纪录.xlsx'
-df_wenjuan = pd.read_excel(all_wenjuanxing,converters={r'1、“一店一码”商户编码':str,
-                                                       r'7、法定代表人身份证号':str})
+# all_wenjuanxing=r'C:\Users\sun\OneDrive\数字淄博\开发-淄博烧烤\20230502-数据\20230502-1430-问卷星全纪录.xlsx'
+# df_wenjuan = pd.read_excel(all_wenjuanxing,converters={r'1、“一店一码”商户编码':str,
+#                                                        r'4、统一社会信用代码':str})
+
+# dict={}
+# for index,row in df_wenjuan.iterrows():
+#     credit=row[r'4、统一社会信用代码']
+#     code=row[r'1、“一店一码”商户编码']
+#     # idNo=row[r'7、法定代表人身份证号']
+#     # print(index)
+#     if not pd.isna(code):
+#         # df_wenjuan.at[index, '数据来源'] = '问卷星'
+#         # df_wenjuan.at[index, '一店一码'] = uniqueid
+#         # print(code,idNo)
+#         dict[credit]=code
+
+# # print(dict)
+
+# path_lv=r'C:\Users\sun\OneDrive\数字淄博\开发-淄博烧烤\20230502-数据\截止到20230502-问卷星导入记录-100.xlsx'
+# df_lv = pd.read_excel(path_lv, sheet_name='20230501-吕金声',
+#                            converters={r'1、“一店一码”商户编码':str,r'4、统一社会信用代码':str})
+
+# for index,row in df_lv.iterrows():
+#     credit=row[r'统一社会信用代码']
+#     # code=row[r'lat']
+#     # idNo=row[r'7、法定代表人身份证号']
+#     # print(index)
+#     if not pd.isna(credit):
+#         # df_wenjuan.at[index, '数据来源'] = '问卷星'
+#         # df_wenjuan.at[index, '一店一码'] = uniqueid
+#         # print(code,idNo)
+#         code = dict.get(credit)
+#         if code is not None:
+#             # print(code)
+#             # df_lv['lat']=code
+#             df_lv.at[index, 'lat'] = code
+#             # print(credit,code)
+
+# df_lv.to_excel(path_lv, sheet_name='20230501-吕金声',index=False)
+
+# print(dict)
+
+
+all_wenjuanxing=r'C:\Users\sun\OneDrive\数字淄博\开发-淄博烧烤\20230502-数据\截止到20230502-问卷星导入记录-1.xlsx'
+df_wenjuan = pd.read_excel(all_wenjuanxing, sheet_name='20230501-吕金声-新整理',
+                           converters={'唯一编码':str,'lat':str})
 
 dict={}
 for index,row in df_wenjuan.iterrows():
-    code=row[r'1、“一店一码”商户编码']
-    idNo=row[r'7、法定代表人身份证号']
+    id=row[r'唯一编码'] #唯一编码
+    code=row[r'lat'] #一店一码
     # print(index)
-    if not pd.isna(code) and not pd.isna(idNo):
-        # df_wenjuan.at[index, '数据来源'] = '问卷星'
-        # df_wenjuan.at[index, '一店一码'] = uniqueid
+    if not pd.isna(id) and not pd.isna(code):
         # print(code,idNo)
-        dict[idNo]=code
+        dict[id]=code
+# df_wenjuan.to_excel(excel_path, index=False)
+# print(dict)
 
-print(dict)
+excel_path=r'C:\Users\sun\OneDrive\数字淄博\开发-淄博烧烤\20230502-数据\20230502-淄博本地生活数-7.xlsx'
+df_table = pd.read_excel(excel_path,converters={'唯一编码':str,'数据版本':str})
+
+for index,row in df_table.iterrows():
+    name=row['经营店铺名称']
+    id=row['唯一编码']
+    # credit=row['统一社会信用代码']
+    uniqueid=dict.get(id)
+    # print(uniqueid)
+    # stamp=row['数据版本']
+    # print(id)
+    if not pd.isna(id) and not pd.isna(uniqueid):
+        # if stamp == '429':
+        df_table.at[index, '数据来源'] = '问卷星'
+        df_table.at[index, '一店一码'] = uniqueid
+        df_table.at[index, '数据版本'] = '501'
+        # print(id,uniqueid,name)
+        # print(id,uniqueid,name)
+
+df_table.to_excel(excel_path, index=False)
