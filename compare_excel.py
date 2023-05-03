752bbe175880401541dba0fdc86c8d4019b63dcf
sun@dorm <65739056@qq.com>
2023-05-03 08:40:40 +0800

    aaa

--- compare_excel.py
+++ compare_excel.py
@@ -19,55 +19,75 @@ def find_equals_row(row_left, row_right):
 
     result=same_column(id_l,id_r)
     # print(result)
-    if result is not None: return result
+    if result is not None: return ('唯一编码',*result)
 
     result=same_column(row_left['经营店铺名称'],row_right['经营店铺名称'])
-    if result is not None: return result
+    if result is not None: return ('经营店铺名称',*result)
 
     result=same_column(row_left['营业执照名称'],row_right['营业执照名称'])
-    if result is not None: return result
+    if result is not None: return ('营业执照名称',*result)
 
     result=same_column(row_left['统一社会信用代码'],row_right['统一社会信用代码'])
-    if result is not None: return result
+    if result is not None: return ('统一社会信用代码',*result)
 
     result=same_column(row_left['法人名称'],row_right['法人名称'])
-    if result is not None: return result
+    if result is not None: return ('法人名称',*result)
 
     result=same_column(row_left['法人手机号'],row_right['法人手机号'])
-    if result is not None: return result
+    if result is not None: return ('法人手机号',*result)
 
     result=same_column(row_left['法人身份证号码'],row_right['法人身份证号码'])
-    if result is not None: return result
+    if result is not None: return ('法人身份证号码',*result)
 
     result=same_column(row_left['食品经营许可证编码'],row_right['食品经营许可证编码'])
-    if result is not None: return result
+    if result is not None: return ('食品经营许可证编码',*result)
 
     result=same_column(row_left['营业类型（例如：个人，个体户，企业）'],row_right['营业类型（例如：个人，个体户，企业）'])
-    if result is not None: return result
+    if result is not None: return ('营业类型（例如：个人，个体户，企业）',*result)
 
     result=same_column(row_left['行业类型（必吃烧烤、鲁菜鲁味、酒店住宿、景点游玩、淄博好品、苍蝇小馆、博山菜、十大人气榜单）'],row_right['行业类型（必吃烧烤、鲁菜鲁味、酒店住宿、景点游玩、淄博好品、苍蝇小馆、博山菜、十大人气榜单）'])
-    if result is not None: return result
+    if result is not None: return ('行业类型（必吃烧烤、鲁菜鲁味、酒店住宿、景点游玩、淄博好品、苍蝇小馆、博山菜、十大人气榜单）',*result)
 
     result=same_column(row_left['店铺联系人'],row_right['店铺联系人'])
-    if result is not None: return result
+    if result is not None: return ('店铺联系人',*result)
 
     result=same_column(row_left['联系邮箱'],row_right['联系邮箱'])
-    if result is not None: return result
+    if result is not None: return ('联系邮箱',*result)
 
     result=same_column(row_left['店铺联系方式（座机）'],row_right['店铺联系方式（座机）'])
-    if result is not None: return result
+    if result is not None: return ('店铺联系方式（座机）',*result)
 
     result=same_column(row_left['店铺联系方式（手机号）'],row_right['店铺联系方式（手机号）'])
-    if result is not None: return result
+    if result is not None: return ('店铺联系方式（手机号）',*result)
 
     result=same_column(row_left['区县'],row_right['区县'])
-    if result is not None: return result
+    if result is not None: return ('区县',*result)
 
-    # result=same_column(row_left['详细地址'],row_right['详细地址'])
-    # if result is not None: return result
+    result=same_column(row_left['详细地址'],row_right['详细地址'])
+    if result is not None: return ('详细地址',*result)
 
-    # result=same_column(row_left['地理纬度'],row_right['地理纬度'])
-    # if result is not None: return result
+    # 如果经纬度小数点后超过了10位，那么表示的精度已经高到超出实际需要的范围，
+    # 因为地球的直径约为12,742公里，在10位小数点的精度下，每个位置的误差已经小于一毫米。
+    # 因此，小数点后超过10位的经纬度已经超出了实际需要的精度范围，而且这样高精度的数据通常也难以收集和处理。
+    # 在一般情况下，经纬度的小数点后保留6位，精确到小数点后的第6位。
+    # 这意味着经度和纬度的精确度为约10米。如果保留更多的小数位，则精度会更高，可以表示更精确的地理位置。
+    # 一般来说，经纬度保留小数点后7位已经足够了，能够满足大多数应用场景的需求。
+    # 这个精度约为10米左右，可以满足大多数位置标识和导航的需要。
+
+    result=same_column(row_left['地理经度'],row_right['地理经度'])
+    if result is not None:
+        # print(abs(result[0]-result[1]))
+        if abs(result[0]-result[1]) < 1e-8:
+            return None
+        else:
+            return ('地理经度',*result)
+
+    result=same_column(row_left['地理纬度'],row_right['地理纬度'])
+    if result is not None: 
+        if abs(result[0]-result[1]) < 1e-8:
+            return None
+        else:
+            return ('地理纬度',*result)
 
     result=same_column(row_left['是否是人气商家'],row_right['是否是人气商家'])
     if result is not None: return ('是否是人气商家',*result)
@@ -96,9 +116,6 @@ def find_equals_row(row_left, row_right):
     result=same_column(row_left['经营店铺名称-原始备份'],row_right['经营店铺名称-原始备份'])
     if result is not None: return ('经营店铺名称-原始备份', *result)
 
-    result=same_column(row_left['详细地址'],row_right['详细地址'])
-    if result is not None: return ('详细地址', *result)
-
     result=same_column(row_left['lat'],row_right['lat'])
     if result is not None: return ('lat', *result)
 
@@ -123,8 +140,8 @@ def find_equals_row(row_left, row_right):
     result=same_column(row_left['删除标志'],row_right['删除标志'])
     if result is not None: return ('删除标志', *result)
 
-    result=same_column(row_left['数据版本'],row_right['数据版本'])
-    if result is not None: return result
+    # result=same_column(row_left['数据版本'],row_right['数据版本'])
+    # if result is not None: return result
 
     # result=same_column(row_left['一店一码'],row_right['一店一码'])
     # if result is not None: return result
